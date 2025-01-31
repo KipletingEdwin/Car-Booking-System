27170c1a0a1663f42ab504a95bfe807cca41cba9
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-31 20:09:39 +0000

    Add getters and setters

--- src/Car.java
+++ src/Car.java
@@ -6,7 +6,59 @@ public class Car {
     private  int noOfAvailableCars;
 
 
+    public String getCarId() {
+        return carId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public int getNoOfAvailableCars() {
+        return noOfAvailableCars;
+    }
+
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public void setNoOfAvailableCars(int noOfAvailableCars) {
+        this.noOfAvailableCars = noOfAvailableCars;
+    }
+
     public Car(){
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
