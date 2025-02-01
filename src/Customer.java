83ecb737ee053d70dbd3725c6881e5e7acd07bbd
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-01 15:32:11 +0000

    Add getters

--- src/Customer.java
+++ src/Customer.java
@@ -3,9 +3,19 @@ public class Customer {
     private  String id;
     private  String name;
 
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public  Customer(String id, String name) {
         this.id = id;
         this.name = name;
+
+
 
     }
 }
