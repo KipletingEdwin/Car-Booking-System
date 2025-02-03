80ce5a87e0e65f758e01c05f5f26873a3865e3f8
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-03 09:44:19 +0000

    Add variables in the constructor

--- src/BookedCarInformation.java
+++ src/BookedCarInformation.java
@@ -6,6 +6,9 @@ public class BookedCarInformation {
 
 
     public  BookedCarInformation(Car car, Customer customer, int days){
+        this.car = car;
+        this.customer = customer;
+        this.days = days;
 
     }
 
